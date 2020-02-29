e8bb5b2a65b06d692379dd4052eb7a71b4e1ddca
Mark <zzkrm55@gmail.com>
2020-02-29 19:38:29 +1000

    Update interpolation syntax

--- main.tf
+++ main.tf
@@ -11,12 +11,12 @@ resource "random_id" "credentials" {
 
 resource "aws_ecs_service" "vpn" {
   name            = "vpn-${random_id.vpn.hex}"
-  cluster         = "${var.cluster["id"]}"
-  task_definition = "${aws_ecs_task_definition.service_task.family}"
+  cluster         = var.cluster["id"]
+  task_definition = aws_ecs_task_definition.service_task.family
   desired_count   = 1
 
   lifecycle {
-    ignore_changes = ["desired_count"]
+    ignore_changes = [desired_count]
   }
 
   ordered_placement_strategy {
@@ -29,7 +29,7 @@ resource "aws_ecs_service" "vpn" {
 
 resource "aws_ecs_task_definition" "service_task" {
   family                = "vpn-${random_id.vpn.hex}"
-  container_definitions = "${data.template_file.service_task.rendered}"
+  container_definitions = data.template_file.service_task.rendered
 
   volume {
     name      = "modules"
@@ -38,27 +38,56 @@ resource "aws_ecs_task_definition" "service_task" {
 }
 
 data "template_file" "service_task" {
-  template = "${file("${path.module}/templates/service_task.json")}"
+  template = file("${path.module}/templates/service_task.json")
 
   vars = {
-    container_name   = "${lookup(var.config, "container_name", lookup(var.defaults, "container_name"))}"
-    container_image  = "${lookup(var.config, "container_image", lookup(var.defaults, "container_image"))}"
-    container_memory = "${lookup(var.config, "container_memory", lookup(var.defaults, "container_memory"))}"
-    container_cpu    = "${lookup(var.config, "container_cpu", lookup(var.defaults, "container_cpu"))}"
-
-    aws_region = "${lookup(var.config, "aws_region", lookup(var.defaults, "aws_region"))}"
-
-    aws_route53_map_public_ip = "${lookup(var.config, "aws_route53_map_public_ip", lookup(var.defaults, "aws_route53_map_public_ip"))}"
-    aws_route53_private_zone  = "${lookup(var.config, "aws_route53_private_zone", lookup(var.defaults, "aws_route53_private_zone"))}"
-    aws_route53_zone          = "${lookup(var.config, "aws_route53_zone", lookup(var.defaults, "aws_route53_zone"))}"
-    aws_route53_record        = "${lookup(var.config, "aws_route53_record", lookup(var.defaults, "aws_route53_record"))}"
-
-    aws_logs_region        = "${lookup(var.cw, "region", lookup(var.config, "aws_region", lookup(var.defaults, "aws_region")) )}"
-    aws_logs_group         = "${lookup(var.cw, "group")}"
-    aws_logs_stream_prefix = "${lookup(var.config, "aws_logs_stream_prefix", lookup(var.defaults, "aws_logs_stream_prefix"))}"
-
-    vpn_ipsec_psk = "${lookup(var.config, "vpn_ipsec_psk", random_id.credentials.0.hex)}"
-    vpn_username  = "${lookup(var.config, "vpn_username", random_id.credentials.1.hex)}"
-    vpn_password  = "${lookup(var.config, "vpn_password", random_id.credentials.2.hex)}"
+    container_name = lookup(var.config, "container_name", var.defaults["container_name"])
+    container_image = lookup(
+      var.config,
+      "container_image",
+      var.defaults["container_image"],
+    )
+    container_memory = lookup(
+      var.config,
+      "container_memory",
+      var.defaults["container_memory"],
+    )
+    container_cpu = lookup(var.config, "container_cpu", var.defaults["container_cpu"])
+    aws_region    = lookup(var.config, "aws_region", var.defaults["aws_region"])
+    aws_route53_map_public_ip = lookup(
+      var.config,
+      "aws_route53_map_public_ip",
+      var.defaults["aws_route53_map_public_ip"],
+    )
+    aws_route53_private_zone = lookup(
+      var.config,
+      "aws_route53_private_zone",
+      var.defaults["aws_route53_private_zone"],
+    )
+    aws_route53_zone = lookup(
+      var.config,
+      "aws_route53_zone",
+      var.defaults["aws_route53_zone"],
+    )
+    aws_route53_record = lookup(
+      var.config,
+      "aws_route53_record",
+      var.defaults["aws_route53_record"],
+    )
+    aws_logs_region = lookup(
+      var.cw,
+      "region",
+      lookup(var.config, "aws_region", var.defaults["aws_region"]),
+    )
+    aws_logs_group = var.cw["group"]
+    aws_logs_stream_prefix = lookup(
+      var.config,
+      "aws_logs_stream_prefix",
+      var.defaults["aws_logs_stream_prefix"],
+    )
+    vpn_ipsec_psk = lookup(var.config, "vpn_ipsec_psk", random_id.credentials[0].hex)
+    vpn_username  = lookup(var.config, "vpn_username", random_id.credentials[1].hex)
+    vpn_password  = lookup(var.config, "vpn_password", random_id.credentials[2].hex)
   }
 }
+
--- variables.tf
+++ variables.tf
@@ -1,49 +1,47 @@
-variable "name" {}
+variable "name" {
+}
 
 variable "env" {
   default = "test"
 }
 
 variable "vpc" {
-  type = "map"
+  type = map(string)
 }
 
 variable "alb" {
-  type = "map"
+  type = map(string)
 }
 
 variable "cluster" {
-  type = "map"
+  type = map(string)
 }
 
 variable "cw" {
-  type    = "map"
+  type    = map(string)
   default = {}
 }
 
 variable "config" {
-  type    = "map"
+  type    = map(string)
   default = {}
 }
 
 variable "defaults" {
-  type = "map"
+  type = map(string)
 
   default = {
-    container_name   = "vpn"
-    container_image  = "hwdsl2/ipsec-vpn-server"
-    container_memory = 128
-    container_cpu    = 128
-
-    aws_region = "us-west-2"
-
+    container_name            = "vpn"
+    container_image           = "hwdsl2/ipsec-vpn-server"
+    container_memory          = 128
+    container_cpu             = 128
+    aws_region                = "us-west-2"
     aws_route53_map_public_ip = true
     aws_route53_private_zone  = false
     aws_route53_zone          = ""
     aws_route53_record        = ""
-
-    aws_logs_stream_prefix = "vpn"
-
-    vpn_daemon_enable = false
+    aws_logs_stream_prefix    = "vpn"
+    vpn_daemon_enable         = false
   }
 }
+
